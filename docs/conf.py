3d8fd28b257a4e7be0a4958ac61ecb3ee8c5d45a
Rolf Verberg <rv43@cornell.edu>
2025-12-23 10:31:46 -0500

    doc: retry testing with autodoc2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 #autodoc_mock_imports = ['any_package_like_numpy_or_list_of_packages']
-autodoc2_packages = ['../src']
+autodoc2_packages = ['../src/']
 autodoc2_render_plugin = 'myst'
 exclude_patterns = ['_build'] 
 source_suffix = ['.rst', '.md']
