e7ef33abae8e5df8077ccf0987c104f7b5cc69d8
Rolf Verberg <rv43@cornell.edu>
2025-12-22 20:07:04 -0500

    doc: testing with autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -20,16 +20,16 @@ author = 'Rolf Verberg'
 
 extensions = [
     'myst_parser',
-#    'sphinx.ext.autodoc',
-    'autodoc2',
+    'sphinx.ext.autodoc',
+#    'autodoc2',
 #    'sphinxarg.ext'
-#    'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',
 ]
 #autodoc_mock_imports = ['any_package_like_numpy_or_list_of_packages']
-autodoc2_packages = ['../src']
-autodoc2_render_plugin = 'myst'
-#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-#source_suffix = ['.rst', '.md']
+#autodoc2_packages = ['../src']
+#autodoc2_render_plugin = 'myst'
+exclude_patterns = ['_build'] 
+source_suffix = ['.rst', '.md']
 templates_path = ['_templates']
 
 
